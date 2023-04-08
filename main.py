de22eae73dbee10d0b56ada9db5f2bcf5c666679
srivastsh <76992595+srivastsh@users.noreply.github.com>
2023-04-08 16:54:31 -0700

    fixing it

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-import openai
 import streamlit as st
+import openai
 from streamlit_chat import message
 
 openai.api_key = st.secrets["api_key"]
 
-
-def hide_style():
-    return """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style>
-        """
-
-
-st.markdown(hide_style(), unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title("CareChat")
 
@@ -23,7 +19,6 @@ if 'generated' not in st.session_state:
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-
 
 def query(prompt):
     response = openai.Completion.create(
@@ -36,31 +31,22 @@ def query(prompt):
     )
     return response.choices[0].text.strip()
 
-
 def get_text():
     input_text = st.text_input("Patient: ")
     return input_text
 
-
-# Placeholder for the text input widget
-text_input_placeholder = st.empty()
-user_input = text_input_placeholder.text_input("Patient: ")
-
+user_input = get_text()
 submit_button = st.button("Ask")
 
 if submit_button and user_input:
-    context = "\n".join([f"Patient: {msg}" if i % 2 == 0 else f"Therapist: {msg}" for i, msg in
-                         enumerate(st.session_state['past'] + st.session_state['generated'])])
+    context = "\n".join([f"Patient: {msg}" if i % 2 == 0 else f"Therapist: {msg}" for i, msg in enumerate(st.session_state['past'] + st.session_state['generated'])])
 
     response = query(f"{context}\nPatient: {user_input}\nTherapist: ")
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(response)
 
-    # Clear the text input after the message is submitted
-    text_input_placeholder.text_input("Patient: ", value="", key="user_input")
-
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated']) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=f"{i}")
-        message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
