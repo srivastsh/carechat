22d12fe4d9ef7cd25a9346d437680937661b8848
srivastsh <76992595+srivastsh@users.noreply.github.com>
2023-04-08 16:31:40 -0700

    cleaned up

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from streamlit_chat import message
 
 openai.api_key = st.secrets["api_key"]
 
-
 def hide_style():
     return """
         <style>
@@ -12,7 +11,6 @@ def hide_style():
         footer {visibility: hidden;}
         </style>
         """
-
 
 st.markdown(hide_style(), unsafe_allow_html=True)
 
@@ -23,7 +21,6 @@ if 'generated' not in st.session_state:
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-
 
 def query(prompt):
     response = openai.Completion.create(
@@ -36,15 +33,12 @@ def query(prompt):
     )
     return response.choices[0].text.strip()
 
-
 def get_text():
     input_text = st.text_input("Patient: ")
     return input_text
 
-
-# Placeholder for the text input widget
 text_input_placeholder = st.empty()
-user_input = text_input_placeholder.text_input("Patient: ")
+user_input = text_input_placeholder.text_input("Patient: ", value="", key="user_input")
 
 submit_button = st.button("Ask")
 
@@ -57,7 +51,6 @@ if submit_button and user_input:
     st.session_state.past.append(user_input)
     st.session_state.generated.append(response)
 
-    # Clear the text input after the message is submitted
     text_input_placeholder.text_input("Patient: ", value="", key="user_input")
 
 if st.session_state['generated']:
